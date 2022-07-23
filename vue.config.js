7817f26fcb575fe322333c88beff8bcf2b19f84a
Jake Utley <j.utley93@gmail.com>
2022-07-22 21:52:24 -0700

    Still adding plugin...

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
+process.env.NODE_ENV === "production" ? "/evil-wordle/" : "/",
 module.exports = defineConfig({
   transpileDependencies: true
 })
